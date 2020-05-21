6667d7ca4a5e2925c84838148c4269c792d766a0
zhanglei <zhanglei@evergrande.com>
2020-05-21 20:19:22 +0800

    no message

--- flink-demo/src/main/scala/com/cloudera/flink/demo/KafkaSinkDemo.scala
+++ flink-demo/src/main/scala/com/cloudera/flink/demo/KafkaSinkDemo.scala
@@ -2,7 +2,7 @@ package com.cloudera.flink.demo
 
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumerBase
+
 
 
 object KafkaSinkDemo {
@@ -10,7 +10,7 @@ object KafkaSinkDemo {
   def main(args: Array[String]): Unit = {
     val streamEnv: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
 
-    streamEnv.addSource(new FlinkKafkaConsumerBase[]() {})
+    streamEnv.addSource(new FlinkKafka)
 
   }
 
